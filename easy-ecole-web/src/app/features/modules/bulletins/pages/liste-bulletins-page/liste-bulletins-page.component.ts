271fc41a968e9e478acf9f1b03c1354c66b97f9b
EasyEcole Dev <dev@easyecole.com>
2026-06-16 09:53:38 +0000

    feat(bulletins): create liste-bulletins-page with filters

--- easy-ecole-web/src/app/features/modules/bulletins/pages/liste-bulletins-page/liste-bulletins-page.component.ts
+++ easy-ecole-web/src/app/features/modules/bulletins/pages/liste-bulletins-page/liste-bulletins-page.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { BulletinService } from '../../services/bulletin.service';
 
 @Component({
   selector: 'app-liste-bulletins-page',
   templateUrl: './liste-bulletins-page.component.html',
   styleUrls: ['./liste-bulletins-page.component.scss']
 })
-export class ListeBulletinsPageComponent {}
+export class ListeBulletinsPageComponent implements OnInit {
+  bulletins = signal<any[]>([]);
+  loading = signal(false);
+  filtres: any = { classeId: '', semestre: '', anneeAcademiqueId: '', statut: '' };
+
+  constructor(private bulletinService: BulletinService) {}
+
+  ngOnInit() {
+    this.charger();
+  }
+
+  charger() {
+    this.loading.set(true);
+    this.bulletinService.getAll(this.filtres).subscribe({
+      next: (data) => this.bulletins.set(data),
+      error: () => this.loading.set(false),
+      complete: () => this.loading.set(false)
+    });
+  }
+
+  trackByFn(index: number, item: any): number {
+    return item.id;
+  }
+
+  getMentionClass(mention: string): string {
+    if (!mention) return '';
+    return mention.toLowerCase().replace(/ /g, '-');
+  }
+}
